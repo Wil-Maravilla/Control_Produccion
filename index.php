a3b469a293678f6e7f843e2dfc6de6efac040ad6
Rafael Maravilla <wilraf.2001@gmail.com>
2021-03-08 20:12:56 -0600

    Agregando estructura base de html y maquetacion con grid css

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="vista/resources/css/estilos.css">
+	<link rel="stylesheet" href="vista/resources/css/template.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>Inicio</title>
+</head>
+<body>
+
+	<header class="header">
+		<h1 class="header__title">Titulo de pagina</h1>
+	</header>
+
+	<aside class="aside">
+		<div class="aside__content">
+				<p class="aside__title--menu">Menu</p>
+				<i class="fa fa-bars aside__title--icon" aria-hidden="true">Button</i>
+		</div>
+		<nav class="menu">
+			<ul class="menu__content">
+				<li class="menu__items"><a href="#" class="menu__items--links">Dashboard</a></li>
+				<li class="menu__items"><a href="#" class="menu__items--links">Usuarios</a></li>
+				<li class="menu__items"><a href="#" class="menu__items--links">Pedidos</a></li>
+				<li class="menu__items"><a href="#" class="menu__items--links">Inventario</a></li>
+				<li class="menu__items"><a href="#" class="menu__items--links">Combinaciones</a></li>
+				<li class="menu__items"><a href="#" class="menu__items--links">Clientes</a></li>
+				<li class="menu__items"><a href="#" class="menu__items--links">Solicitudes de materia prima</a></li>
+				<li class="menu__items"><a href="#" class="menu__items--links">Proveedores</a></li>
+			</ul>
+		</nav>
+	</aside>
+
+	<section class="contenedor">
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing, elit. Explicabo itaque vel blanditiis cumque consectetur quod aliquid sequi error, libero animi nam sint, magni. Excepturi earum eveniet praesentium, fugit cumque doloribus.</p>
+	</section>
+
+	<footer class="pie">Pie de pagina</footer>
+</body>
+</html>
