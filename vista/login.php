c9b8899dd6657170199c83da5520997b8294d712
Rafael Maravilla <wilraf.2001@gmail.com>
2021-03-14 17:10:29 -0600

    Diseñando el login

---
+++ vista/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="resources/css/estilos_login.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Karla&display=swap" rel="stylesheet">
+	<title>Login</title>
+</head>
+<body>
+
+	<div class="container-login">
+		<div class="container-header">
+			<h1 class="header-title">Inicio de sesion</h1>
+		</div>
+		<form action="#" class="form">
+				<label for="usuario" class="form__label">Usuario</label>
+				<input type="text" class="form__text" id="usuario" required>
+				<label for="contrasenia" class="form__label">Contraseña</label>
+				<input type="password" class="form__text" id="contrasenia" required>
+
+			<input type="submit" class="form__submit" value="Iniciar Sesion">
+		</form>
+	</div>
+
+</body>
+</html>
